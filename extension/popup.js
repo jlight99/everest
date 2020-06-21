8e0c805ce615d42790052f16f3c0821ff70cd89a
jlight99 <ellenhuang523@gmail.com>
2020-06-21 05:10:54 -0400

    add some bootstrap, implement add to blacklist

--- extension/popup.js
+++ extension/popup.js
@@ -1,11 +1,11 @@
 let openGraphs = document.getElementById('openGraphs');
 
-openGraphs.onclick = function(element) {
+openGraphs.addEventListener("click", function(element) {
   chrome.tabs.create({url: chrome.extension.getURL('graphs.html')});
-}
+});
 
 let openSettings = document.getElementById('openSettings');
 
-openSettings.onclick = function(element) {
+openSettings.addEventListener("click", function(element) {
   chrome.tabs.create({url: chrome.extension.getURL('settings.html')});
-}
+});
--- extension/settings.js
+++ extension/settings.js
@@ -1,16 +1,43 @@
-OUR_APP_BLACKLIST_KEY = "our_appname_blacklist_key";
+var blacklistKey = "our_appname_blacklist_key";
 getBlackList(function(blacklist) {
   if (!blacklist) {
     blacklist = ['facebook', 'youtube', 'fanfiction'];
     console.log('blacklist not found!');
-  } else {
-    blacklist = blacklist[blacklistKey];
   }
   var blacklistElement = document.getElementById('blacklist');
   for (var i = 0; i < blacklist.length; i++) {
     var item = blacklist[i];
     var li = document.createElement('li');
+    li.className = "list-group-item";
     li.appendChild(document.createTextNode(item));
     blacklistElement.appendChild(li);
   }
+});
+
+var addToBlacklistForm = document.getElementById('add-input');
+
+var addDomainField = document.getElementById('add-domain-field');
+
+var addToBlacklistBtn = document.getElementById('add-to-blacklist');
+addToBlacklistBtn.addEventListener("click", function() {
+  addToBlacklistForm.classList.remove('hidden');
+  addToBlacklistBtn.classList.add('hidden');
+});
+
+var submitAddDomainBtn = document.getElementById('submit-add-domain');
+submitAddDomainBtn.addEventListener("click", function(evt) {
+  addToBlacklistForm.classList.add('hidden');
+  addToBlacklistBtn.classList.remove('hidden');
+  addToBlackList(addDomainField.value, function() {
+    var li = document.createElement('li');
+    li.className = "list-group-item";
+    li.appendChild(document.createTextNode(addDomainField.value));
+    document.getElementById('blacklist').appendChild(li);
+  });
+});
+
+var cancelAddDomainBtn = document.getElementById('cancel-add-domain');
+cancelAddDomainBtn.addEventListener("click", function() {
+  addToBlacklistForm.classList.add('hidden');
+  addToBlacklistBtn.classList.remove('hidden');
 });
